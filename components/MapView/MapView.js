be7c040b3f31470ec4eba81e4414d5b24367c10b
Aleksandra Gradel <aleksandra.bado@gmail.com>
2022-08-23 11:50:14 +0400

    Update MapView.js

--- components/MapView/MapView.js
+++ components/MapView/MapView.js
@@ -1,11 +1,29 @@
 import Map, {Marker, Popup} from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
+import Link from 'next/link'
 
 const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN
 
 function MapView({ places }) {
-    const [popupOpen, setPopupOpen] = useState(false)
+    const [popupInfo, setPopupInfo] = useState(null)
+
+    const markers = useMemo(() => places?.map((place, index) => (
+        <Marker 
+            key={`marker-${index}`}
+            latitude={place.geolocation.lat} 
+            longitude={place.geolocation.lng}
+            onClick={e => {
+                e.originalEvent.stopPropagation();
+                setPopupInfo(place);
+              }}>
+            <span className='bg-white text-grey-dark font-bold shadow-md p-2 rounded-full text-md'>
+                ${place.pricePerNight}
+            </span>
+        </Marker>
+    )), [])
+
+    console.log(popupInfo)
     
     return (
         <div>
@@ -19,28 +37,20 @@ function MapView({ places }) {
                 mapStyle="mapbox://styles/mapbox/light-v10"
                 mapboxAccessToken={MAPBOX_TOKEN}
             >
-                { places?.map((place, index) => place.slug && place.geolocation && (
-                    <div key='place._id'>
-                        <Marker 
-                            key={index}
-                            latitude={place.geolocation.lat} 
-                            longitude={place.geolocation.lng}
-                            onClick={() => setPopupOpen(true)}>
-                            <span className='bg-white text-grey-dark font-bold shadow-md p-2 rounded-full text-md'>
-                                ${place.pricePerNight}
-                            </span>
-                        </Marker>
-                        { popupOpen && (
-                            <Popup
-                                key={index}
-                                longitude={place.geolocation.lng} 
-                                latitude={place.geolocation.lat}
-                                onClose={() => setPopupOpen(false)}>
-                                You are here
-                            </Popup>
-                        )}
-                    </div>
-                ))}
+                {markers}
+
+                {popupInfo && (
+                    <Popup
+                        longitude={popupInfo.geolocation.lng} 
+                        latitude={popupInfo.geolocation.lat}
+                        onClose={() => setPopupInfo(null)}>
+                        <Link href={`place/${popupInfo.slug.current}`} key={popupInfo?._id}>
+                            <a>
+                                {popupInfo?.title}
+                            </a>
+                        </Link>
+                    </Popup>
+                )}
             </Map>
         </div>
     )
